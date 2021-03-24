244e2da1ff9a984f5523f999446aca1e6d65ee70
Diego Manrique <diegomanri@gmail.com>
2021-03-23 22:14:38 -0500

    Adding another solution for outlier identifier

---
+++ findoutlier2.py
@@ -0,0 +1,7 @@
+def find_outlier(int):
+    odds = [x for x in int if x%2!=0]
+    evens= [x for x in int if x%2==0]
+    return odds[0] if len(odds)<len(evens) else evens[0]
+   
+dtest = [12,14,15,16,18]
+print(find_outlier(dtest))
